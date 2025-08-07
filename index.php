76e2b094207ab822d9860f3dc9b07ba69f7ba2fc
Ishan744 <isankarmacharya63@gmail.com>
2025-08-07 10:29:29 +0545

    css setup

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+
 <?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>hello</h1>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+</html
